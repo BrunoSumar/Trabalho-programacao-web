d3755659670a3b8956c8a863c6cfc456294dec7e
Bruno <sumar.bruno@yahoo.com>
2020-11-14 18:14:04 -0300

    check nick email

--- API/controllers/userController.php
+++ API/controllers/userController.php
@@ -7,9 +7,9 @@ $data = ['Error'];
 
 if (isset($_POST["action"])) {
     if ($_POST["action"] == 'create') {
-        $nickname = $_POST['nickname'];
-        $email = $_POST['email'];
-        $password = $_POST['password'];
+        $nickname = trim($_POST['nickname']);
+        $email = trim($_POST['email']);
+        $password = trim($_POST['password']);
 
         $data = $user_object->create_user($nickname, $email, $password);
     }
@@ -18,14 +18,25 @@ if (isset($_POST["action"])) {
         $identifier =  trim($_POST['identifier']);
         $password = trim($_POST['password']);
 
-        if(strlen($password <= 255)){
-            if($id = filter_var($identifier, FILTER_VALIDATE_EMAIL)){
-                $data = $user_object->create_validate_by_email($id, $password);
-            }else{
-                $data = $user_object->create_validate_by_nick($identifier, $password);
-            }
+        if($id = filter_var($identifier, FILTER_VALIDATE_EMAIL)){
+            $data = $user_object->create_validate_by_email($id, $password);
+        }else{
+            $data = $user_object->create_validate_by_nick($identifier, $password);
         }
     }
+
+    if ($_POST["action"] == 'check_nick') {
+        $nickname = $_POST['nickname'];
+
+        $data = $user_object->is_nickname_avaible($nickname);
+    }
+
+    if ($_POST["action"] == 'check_email') {
+        $email = $_POST['email'];
+
+        $data = $user_object->is_email_avaible($email);
+    }
+
 }
 
 unset($user_object);
