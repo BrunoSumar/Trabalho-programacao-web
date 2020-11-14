4abe661a5cd72c223a2739e9bd5d563073a19bc3
Bruno <sumar.bruno@yahoo.com>
2020-11-14 17:43:24 -0300

    create validate

--- API/controllers/userController.php
+++ API/controllers/userController.php
@@ -5,7 +5,28 @@ header('Content-Type: application/json;charset=utf-8');
 $user_object = new User();
 $data = ['Error'];
 
-if (isset($_GET["action"])) {
+if (isset($_POST["action"])) {
+    if ($_POST["action"] == 'create') {
+        $nickname = $_POST['nickname'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+
+        $data = $user_object->create_user($nickname, $email, $password);
+    }
+
+    if ($_POST["action"] == 'validate') {
+        $identifier =  trim($_POST['identifier']);
+        $password = trim($_POST['password']);
+
+        if(strlen($password <= 255)){
+            if($id = filter_var($identifier, FILTER_VALIDATE_EMAIL)){
+                $data = $user_object->create_validate_by_email($id, $password);
+            }else{
+                $data = $user_object->create_validate_by_nick($identifier, $password);
+            }
+        }
+    }
 }
+
 unset($user_object);
 echo json_encode($data);
