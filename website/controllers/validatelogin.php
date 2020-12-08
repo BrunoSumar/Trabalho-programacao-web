06b6cbdc00248ea589f852e02d41bab0b51ce259
Bruno <sumar.bruno@yahoo.com>
2020-12-08 11:13:49 -0300

    a

--- website/controllers/validatelogin.php
+++ website/controllers/validatelogin.php
@@ -4,7 +4,7 @@ print_r($_SESSION);
 //session_destroy();
 if(isset($_SESSION['loginNonce']) && ARGS_URL[1]==$_SESSION['loginNonce']){
     $_SESSION['logged'] = true;
-    unset($s['loginNonce']);
+    unset($_SESSION['loginNonce']);
     include __DIR__."/../mybookmarks.html";
 }
 else
