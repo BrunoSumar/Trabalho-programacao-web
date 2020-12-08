0cdf20615c01204b723c1b35ddf805b164928463
Stevemberg <Stevemberg100@gmail.com>
2020-12-08 11:33:40 -0300

    Update index.php and validatelogin.php

--- website/controllers/validatelogin.php
+++ website/controllers/validatelogin.php
@@ -1,11 +1,13 @@
 <?php
 
-print_r($_SESSION);
 //session_destroy();
 if (isset($_SESSION['loginNonce']) && ARGS_URL[1]==$_SESSION['loginNonce']) {
     $_SESSION['logged'] = true;
+    print_r($_SESSION);
     unset($_SESSION['loginNonce']);
-    include __DIR__."/../mybookmarks.html";
+    // include __DIR__."/../mybookmarks.html";
+    header('Location: http://localhost/UFF/Trabalho-programacao-web1/website/mybookmarks');
 } else {
-    include __DIR__."/../home.html";
+    header('Location: http://localhost/UFF/Trabalho-programacao-web1/website/login');
+    // include __DIR__."/../home.html";
 }
